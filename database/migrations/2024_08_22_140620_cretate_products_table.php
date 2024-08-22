3499740af7143ec644263b678f1e97bb4f3a0455
Elias Galvão Alves <galvaoalveselias@gmail.com>
2024-08-22 11:19:57 -0300

    criando e configurando produtos da migration

---
+++ database/migrations/2024_08_22_140620_cretate_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            //Id do registro, autoincrement e primary-key
+            $table->bigIncrements('id');
+
+            //Nome do produto (name) - string
+            $table->string('name');
+
+            //Código do produto (code) - string - não pode repetir
+            $table->string('code')->unique();
+            
+            //Quantidade do produto - () - int - por default quantidade = 0\
+            $table->integer('quantity')->default(0);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
